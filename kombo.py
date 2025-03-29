e705ca4548a1fc7edf0765770db1da5d444d9034
oisteing <66906363+oisteing@users.noreply.github.com>
2025-03-29 12:09:40 +0100

    Update kombo.py

--- kombo.py
+++ kombo.py
@@ -26,26 +26,34 @@ translations = {
     "Nynorsk": {
         "vel_modell": "Vel modell:",
         "intro": "Denne webappen er basert på dei fire tellemåtane i kombinatorikk.",
+        "ordna": "Ordna utval",
+        "uordna": "Uordna utval",
+        "utan": "utan tilbakeleggjing",
+        "med": "med tilbakeleggjing",
         "ordna_utan": "Ordna utval utan tilbakeleggjing",
         "ordna_med": "Ordna utval med tilbakeleggjing",
         "uordna_utan": "Uordna utval utan tilbakeleggjing",
         "uordna_med": "Uordna utval med tilbakeleggjing",
-        "ordna_utan_tekst": "Her skal du gjere r valg på eit utfallsrom som har n moglege når du starter, men der du ikkje legg tilbake.  \nSkriv inn n og så r",
-        "ordna_med_tekst": "Da skal du gjere r valg ut frå n moglegheitar.  \n Eksempel er den gammeldagse fotballkupongen. Skriv inn n og så r",
-        "uordna_utan_tekst": "Her skal du gjere r valg på eit utfallsrom som har n moglege når du starter, men der du ikkje legg tilbake. Eksempel er lottokupongen. \nSkriv inn n og så r",
-        "uordna_med_tekst": "Her skal du gjere r valg på eit utfallsrom som har n moglege, og du legg tilbake det du trekkjer ut.  \nSkriv inn n og så r",
+        "ordna_utan_tekst": "Her skal du gjere r valg på ei mengd som har n moglege når du starter, men der du ikkje legg tilbake.  \nSkriv inn n og så r",
+        "ordna_med_tekst": "Da skal du gjere r valg ut frå n moglegheitar.  \n Eksempel er den gammaldagse fotballkupongen. Skriv inn n og så r",
+        "uordna_utan_tekst": "Her skal du gjere r valg på ei mengd som har n moglege når du starter, men der du ikkje legg tilbake. Eksempel er lottokupongen. \nSkriv inn n og så r",
+        "uordna_med_tekst": "Her skal du gjere r valg på ei mengd som har n moglege, og du legg tilbake det du trekkjer ut.  \nSkriv inn n og så r",
     },
     "Bokmål": {
         "vel_modell": "Velg modell:",
         "intro": "Denne webappen er basert på de fire tellemåtene i kombinatorikk.",
+        "ordna": "Ordnet utvalg",
+        "uordna": "Uordnet utvalg",
+        "utan": "uten tilbakelegging",
+        "med": "med tilbakelegging",
         "ordna_utan": "Ordnet utvalg uten tilbakelegging",
         "ordna_med": "Ordnet utvalg med tilbakelegging",
         "uordna_utan": "Uordnet utvalg uten tilbakelegging",
         "uordna_med": "Uordnet utvalg med tilbakelegging",
-        "ordna_utan_tekst": "Her skal du gjøre r valg på et utfallsrom som har n mulige når du starter, men der du ikke legger tilbake.  \nSkriv inn n og så r",
-        "ordna_med_tekst": "Her skal du foreta r valg på et utfallsrom som har n mulige. Her legger du tilbake det du trekker ut.  \nSkriv inn n og så r",
-        "uordna_utan_tekst": "Her skal du ta r valg på et utfallsrom som har n mulige når du starter, men der du ikke legger tilbake.  \nSkriv inn n og så r",
-        "uordna_med_tekst": "Her skal du foreta r valg på et utfallsrom som har n mulige, men her legger du tilbake det du trekker ut.  \nSkriv inn n og så r",
+        "ordna_utan_tekst": "Her skal du gjøre r valg på en mengde som har n mulige når du starter, men der du ikke legger tilbake.  \nSkriv inn n og så r",
+        "ordna_med_tekst": "Her skal du foreta r valg på en mengde som har n mulige. Her legger du tilbake det du trekker ut.  \nSkriv inn n og så r",
+        "uordna_utan_tekst": "Her skal du ta r valg på en mengde som har n mulige når du starter, men der du ikke legger tilbake.  \nSkriv inn n og så r",
+        "uordna_med_tekst": "Her skal du foreta r valg på en mengde som har n mulige, men her legger du tilbake det du trekker ut.  \nSkriv inn n og så r",
     }
 }
 
@@ -53,24 +61,44 @@ st.title("Kombinatorikk")
 st.sidebar.write(translations[selection]['intro'])
 
 #st.logo("https://www.oisteing.com/images/logowww.png", link="https://www.oisteing.com")
-st.logo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXL74BGleH6oFgRBkKovXDFNTb0h5U46mhnQ&s", link="https://www.github.com")
+#st.logo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXL74BGleH6oFgRBkKovXDFNTb0h5U46mhnQ&s", link="https://www.github.com")
 
 
-valg = st.selectbox(
+#valg = st.selectbox(
+#    translations[selection]['vel_modell'],
+#    (
+#    translations[selection]['ordna_utan'], 
+#    translations[selection]['ordna_med'], 
+#    translations[selection]['uordna_utan'], 
+#    translations[selection]['uordna_med']),
+#)
+#st.divider()
+
+
+# Radioknapp for utvalgstype
+utvalgstype = st.radio(
     translations[selection]['vel_modell'],
-    (
-    translations[selection]['ordna_utan'], 
-    translations[selection]['ordna_med'], 
-    translations[selection]['uordna_utan'], 
-    translations[selection]['uordna_med']),
+    (translations[selection]['uordna'], translations[selection]['ordna'])
 )
-st.divider()
+
+# Avkrysningsboks for tilbakelegging
+med_tilbakelegging = st.checkbox(translations[selection]['med'])
+
+if utvalgstype==translations[selection]['uordna'] and med_tilbakelegging==True:
+    valg = translations[selection]['uordna_med']
+if utvalgstype==translations[selection]['uordna'] and med_tilbakelegging==False:
+    valg = translations[selection]['uordna_utan']
+if utvalgstype==translations[selection]['ordna'] and med_tilbakelegging==True:
+    valg = translations[selection]['ordna_med']
+if utvalgstype==translations[selection]['ordna'] and med_tilbakelegging==False:
+    valg = translations[selection]['ordna_utan']
+
 
 if valg==translations[selection]['ordna_utan']:
     b=1
     st.header(translations[selection]['ordna_utan'])
     st.write(translations[selection]['ordna_utan_tekst'])
-    a=int(st.number_input("n: ", key=1, value=1, min_value=b))
+    a=int(st.number_input("n: ", key=1, value=1, min_value=b, max_value=100))
     b=int(st.number_input("r: ", value=a, min_value=1, max_value=a))
     translations["Bokmål"]['ordna_utan_svar']="Her har du "+ str(ordna_uten(a,b))+ " muligheter."
     translations["Nynorsk"]['ordna_utan_svar']="Her har du "+ str(ordna_uten(a,b))+ " moglegheitar."
@@ -80,7 +108,7 @@ if valg==translations[selection]['ordna_utan']:
 if valg==translations[selection]['ordna_med']:
     st.header(translations[selection]['ordna_med'])
     st.write(translations[selection]['ordna_med_tekst'])
-    a=int(st.number_input("n: ", key=2, value=1, min_value=1))
+    a=int(st.number_input("n: ", key=2, value=1, min_value=1, max_value=100))
     b=int(st.number_input("r: ", value=1, min_value=1))
     translations["Bokmål"]['ordna_med_svar']="Her har du "+ str(ordna_med(a,b))+ " muligheter."
     translations["Nynorsk"]['ordna_med_svar']="Her har du "+ str(ordna_med(a,b))+ " moglegheitar."
